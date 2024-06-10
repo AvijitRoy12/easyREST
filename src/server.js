332492ab94d747f4a051fcf5ea06ec883872dd09
Avijit <contact.roy17@gmail.com>
2024-06-10 23:42:07 +0200

    GET Api created.

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { StatusCodes } from "http-status-codes";
+
+const app = express();
+const port = 3001;
+
+app.get("/test", (req, res) => {
+  res.status(StatusCodes.OK);
+  res.send("Hello You!");
+});
+
+app.post("/add", (req, res) => {
+  res.status(StatusCodes.CREATED);
+  res.send("Nothing is added yet until you integrate database.");
+});
+
+app.listen(port, () => {
+  console.log(`The server is running on ${port}`);
+});
